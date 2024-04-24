efe1011539f9836a759a7bb7b3848ca1bfee91c1
Joseph Chan <jojosfdev@gmail.com>
2024-04-24 15:15:20 -0700

    lecture 29

--- home/react-refresher-01-react-starting-project/src/App.js
+++ home/react-refresher-01-react-starting-project/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import './App.css';
+
+import BasicForm from './components/BasicForm';
 
 const App = () => {
-  return <h1>A React App!</h1>;
+  return <div className='shellFunctionalArea'>
+    <div className='mainContent'>
+      <h2>Some React Advice!</h2>
+      <p>This is some paragraph text</p>
+      <BasicForm />
+    </div>
+  </div>;
 };
 
 export default App;
---
+++ home/react-refresher-01-react-starting-project/src/components/BasicForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BasicForm = () => {
+    return (<form action="">
+        <div className="content--flex-row">
+            <label>Test Input Row</label>
+            <input name="bababooey" />
+        </div>
+
+    </form>
+    );
+};
+
+export default BasicForm;
