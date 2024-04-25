31f42ae7292a62b7bacd61990f72c9e2d644db4c
Joseph Chan <jojosfdev@gmail.com>
2024-04-25 16:02:45 -0700

    Shell components and jsx
    prototype html design for web template

--- home/react-refresher-01-react-starting-project/src/App.js
+++ home/react-refresher-01-react-starting-project/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
 
-import BasicForm from './components/BasicForm';
+import BasicForm from './components/BasicForm.js';
 
 const App = () => {
-  return <div className='shellFunctionalArea'>
-    <div className='mainContent'>
-      <h2>Some React Advice!</h2>
-      <p>This is some paragraph text</p>
-      <BasicForm />
+  return <div className='shell--main'>
+    <div className='shell--banner'>
+      <div className='functional-area'></div>
     </div>
+    <div className='shell--content'>
+      <div className='mainContent'>
+        <h2>Some React Advice!</h2>
+        <p>This is some paragraph text</p>
+        <BasicForm />
+      </div>
+    </div>
+    <div className='shell--footer'></div>
+
   </div>;
 };
 
